b82eee77597c26d6fae7e70380cf0d4b107da650
Hajar <hajar2000alfarsi@gmail.com>
2026-07-27 12:43:24 +0400

    Initialize project

---
+++ src/main/java/com/example/mroojBE/MroojBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mroojBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MroojBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MroojBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mroojBE/MroojBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mroojBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MroojBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
